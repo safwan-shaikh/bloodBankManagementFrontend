750e96e06e2881ee3e8b60492ac12d8cdc498a28
Sonia Kasar <92852957+kasarsonia@users.noreply.github.com>
2023-04-29 00:38:07 +0530

    created getAllDonars function

--- blood_bank_management_frontend/src/components/donations.js
+++ blood_bank_management_frontend/src/components/donations.js
@@ -1,5 +1,15 @@
 import React , { useEffect, useState }from "react";
 import { useDispatch } from "react-redux";
+import LoadingBar from "react-top-loading-bar";
+import { TextField, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, IconButton, Autocomplete, Stack, Grid } from "@mui/material";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { setPopupState } from "../actions";
+import CustomTable from "./customTable";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import moment from "moment";
 
 const Donation = () => {
     const dispatch = useDispatch();
@@ -26,6 +36,54 @@ const Donation = () => {
         blood_bag: null
 
     });
+    const [donation, setDonation] = useState({
+        quantity_ml: 0,
+        bank: null
+    });
+    const today = new Date();
+    const fiveYearsAgo = new Date();
+    fiveYearsAgo.setFullYear(today.getFullYear() - 5);
+
+    const [donorFilters, setdonorFilters] = useState({
+        first_name: '',
+        last_name: '',
+        gender: '',
+        blood_type: '',
+        phone: '',
+        email: '',
+        dob: '',
+        created_at: new Date(),
+        country: '',
+        state: '',
+        city: '',
+        locality: '',
+
+    });
+    const [allDonors, setAllDonors] = useState([])
+    const [allBanks, setAllBanks] = useState([])
+    const [allHospitals, setAllHospitals] = useState([])
+    const [allBankDonors, setAllBankDonors] = useState([])
+
+    useEffect(() => {
+        getAllDonors(donorFilters);
+    }, [donorFilters]);
+
+    const getAllDonors = (donorFilters, callback) => {
+        axios.post('http://localhost:8080/api/bloodBag/getBloodBags', { filters: donorFilters })
+            .then((res) => {
+                setAllDonors(res?.data?.message || []);
+                callback && callback();
+            })
+            .catch(({ response }) => {
+                dispatch(setPopupState({ status: 'show', message: response?.data?.message || 'Something Went Wrong!', type: 'response' }));
+            });
+    }
+    const hUpdate = (index) => {
+        setEditMode(true);
+        setdonationDetails(allDonors[index] || donationDetails);
+        setOpen(true);
+    }
+
     return (
         <>
         </>

82e43218f24eb8bf11c0de0aea14cb07662d7fcc
rm-bias <64217960+rm-bias@users.noreply.github.com>
2023-04-28 19:38:53 +0000

    blood bank details

--- blood_bank_management_frontend/src/components/bloodBank.js
+++ blood_bank_management_frontend/src/components/bloodBank.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 
 const BloodBank = () => {
+
+    const dispatch = useDispatch();
+    const [progress, setProgress] = useState(0);
+
+    const [open, setOpen] = useState(false);
+    const [editMode, setEditMode] = useState(false);
+    const [deleteBank, setDeleteBank] = useState({ open: false, bname: '', bid: 0 });
+    const [bankDetails, setBankDetails] = useState({
+        b_name: '',
+        phone: '',
+        email: '',
+        country: '',
+        state: '',
+        city: '',
+        locality: '',
+
+    });
 
     return (
         <>

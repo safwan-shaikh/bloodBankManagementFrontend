3b58010b7301e65121f1d39ce411e86db9930a41
Sonia Kasar <92852957+kasarsonia@users.noreply.github.com>
2023-04-28 23:00:00 +0530

    Created const variables for donationDetails

--- blood_bank_management_frontend/src/components/donations.js
+++ blood_bank_management_frontend/src/components/donations.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React , { useEffect, useState }from "react";
+import { useDispatch } from "react-redux";
 
 const Donation = () => {
+    const dispatch = useDispatch();
+    const [progress, setProgress] = useState(0);
+    const [open, setOpen] = useState(false);
+    const [editMode, setEditMode] = useState(false);
+    const [deleteDonor, setDeleteDonor] = useState({ open: false, bname: '', bid: 0 });
+    const [donationDetails, setdonationDetails] = useState({
+        first_name: '',
+        last_name: '',
+        gender: '',
+        blood_type: '',
+        phone: '',
+        email: '',
+        dob: null,
+        created_at: new Date(),
+        country: '',
+        state: '',
+        city: '',
+        locality: '',
+        hospital: null,
+        bank: null,
+        quantity_ml: null,
+        blood_bag: null
+
+    });
     return (
         <>
         </>
